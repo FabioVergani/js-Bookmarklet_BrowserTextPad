c964dd1dbb6e72cdc017233bd7b2bb1c3c90aee6
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-08-20 03:51:44 -0700

    Create BasicTextPad-min.js

---
+++ BasicTextPad-min.js
@@ -0,0 +1,1 @@
+javascript:void((w=>{'use strict';const d=w.document,f=()=>{const b=d.body;b.spellcheck=false;b.contentEditable=true};if('complete'!==d.readyState){const l=x=>{d[x+'EventListener']('load',g,true)},g=e=>{l('remove');f()};l('add')}else{f()}})(window.open()));
